15c7a76d2ba86f3de4b71b3d5739288453c24c30
fabian <fabian.vanrissenbeck@tu-dortmund.de>
2022-08-15 19:43:43 +0200

    added open rom dialog and media buttons

--- src/appwindow.c
+++ src/appwindow.c
@@ -2,12 +2,45 @@
 
 struct _C8AppWindow {
     AdwApplicationWindow parent;
+    GtkWidget* open_rom;
+    GtkWidget* start_pause;
+    GtkWidget* stop;
 };
 
 G_DEFINE_TYPE(C8AppWindow, c8_app_window, ADW_TYPE_APPLICATION_WINDOW)
 
+static void c8_app_window_load_rom(C8AppWindow* self, GFile* file);
+
+static void on_rom_select(GtkNativeDialog* native, int response, void* user_data) {
+    if (response == GTK_RESPONSE_ACCEPT) {
+        GtkFileChooser* chooser = GTK_FILE_CHOOSER(native);
+        GFile* file = gtk_file_chooser_get_file(chooser);
+
+        c8_app_window_load_rom(C8_APP_WINDOW(user_data), file);
+    }
+
+    g_object_unref(native);
+}
+
+static void on_open_rom(GtkButton* button, void* user_data) {
+    C8AppWindow* self = C8_APP_WINDOW(user_data);
+
+    GtkFileChooserNative* file_chooser = gtk_file_chooser_native_new(
+        "Open ROM",
+        GTK_WINDOW(self),
+        GTK_FILE_CHOOSER_ACTION_OPEN,
+        "_Open",
+        "_Cancel"
+    );
+
+    g_signal_connect(file_chooser, "response", G_CALLBACK(on_rom_select), self);
+    gtk_native_dialog_show(GTK_NATIVE_DIALOG(file_chooser));
+}
+
 static void c8_app_window_init(C8AppWindow* self) {
     gtk_widget_init_template(GTK_WIDGET(self));
+
+    g_signal_connect(self->open_rom, "clicked", G_CALLBACK(on_open_rom), self);
 }
 
 static void c8_app_window_class_init(C8AppWindowClass* class) {
@@ -15,6 +48,31 @@ static void c8_app_window_class_init(C8AppWindowClass* class) {
         GTK_WIDGET_CLASS(class),
         "/com/github/vrfabian/chip8/res/appwindow.xml"
     );
+
+    gtk_widget_class_bind_template_child_full(
+        GTK_WIDGET_CLASS(class),
+        "open-rom-button",
+        true,
+        G_STRUCT_OFFSET(C8AppWindow, open_rom)
+    );
+
+    gtk_widget_class_bind_template_child_full(
+        GTK_WIDGET_CLASS(class),
+        "emu-start-pause-button",
+        true,
+        G_STRUCT_OFFSET(C8AppWindow, start_pause)
+    );
+
+    gtk_widget_class_bind_template_child_full(
+        GTK_WIDGET_CLASS(class),
+        "emu-stop-button",
+        true,
+        G_STRUCT_OFFSET(C8AppWindow, stop)
+    );
+}
+
+static void c8_app_window_load_rom(C8AppWindow* self, GFile* file) {
+    g_print("Loading rom...\n");
 }
 
 C8AppWindow* c8_app_window_new(void) {
